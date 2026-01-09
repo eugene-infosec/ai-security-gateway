4bcf3e6c490bb31f936e33c01fd25cdaf2039096
eugene <eugene.infosec@gmail.com>
2026-01-09 05:27:43 -0800

    chore: apply terraform formatting

--- infra/terraform/cognito.tf
+++ infra/terraform/cognito.tf
@@ -15,8 +15,8 @@ resource "aws_cognito_user_pool" "pool" {
     name                = "tenant_id"
     attribute_data_type = "String"
     # developer_only_attribute = false # Optional default
-    mutable             = false # <--- LOCKED
-    required            = false
+    mutable  = false # <--- LOCKED
+    required = false
     string_attribute_constraints {
       min_length = 1
       max_length = 64
